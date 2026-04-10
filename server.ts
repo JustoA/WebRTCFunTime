22022e5a692b65fbd899ab3258629240e6d1f3b7
Justin Aquilante <JWaquilante@gmail.com>
2026-04-10 15:54:32 -0400

    this does not work

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import type { ServerWebSocket } from "bun";
+import {v4 as uuidv4, type UUIDTypes} from "uuid";
 
-const clients = new Set<ServerWebSocket<unknown>>();
+const clientsByUUID = new Map<string, ServerWebSocket<unknown>>();
+const clientsByWebSocket = new Map<ServerWebSocket<unknown>, string>();
 
 Bun.serve({
   port: 3000,
@@ -21,20 +23,41 @@ Bun.serve({
   },
 
   websocket: {
-    open(ws) {
-      clients.add(ws);
-    },
+    open(ws: ServerWebSocket<undefined>) {
+      // i hate typescript
+      const id = uuidv4();
+      clientsByUUID.set(id,ws);
+      clientsByWebSocket.set(ws,id);
+      console.log("New peer just joined. " + id)
+      console.log("Clients: "+ Array.from(clientsByUUID.keys()))
+      
+      // give client its own id as well as the current list of connected people 
+      ws.send(JSON.stringify({ type: "init", id , peers: "guh"}));
 
-    message(ws, message) {
-      for (const client of clients) {
-        if (client !== ws) {
-          client.send(message);
+      // let everyone know about the new peer
+      for (const [otherId, client] of clientsByUUID){
+        if (otherId !== id){
+          console.log("Notifying " + otherId + " about new peer "+  id)
+          client.send(JSON.stringify({type: "new-peer", id}))
         }
       }
     },
 
+    message(ws, message) {
+      const data = JSON.parse(message.toString());
+      console.log(data)
+      if (data.to && clientsByUUID.has(data.to)){
+          clientsByUUID.get(data.to)?.send(JSON.stringify(data));
+      }
+    },
+
     close(ws) {
-      clients.delete(ws);
+      let uuidToDelete = clientsByWebSocket.get(ws);
+      if(uuidToDelete){
+        console.log("Dropped peer " + uuidToDelete)
+        clientsByWebSocket.delete(ws)
+        clientsByUUID.delete(uuidToDelete);
+      } 
     }
   }
 });
